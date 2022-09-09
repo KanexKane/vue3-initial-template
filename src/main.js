6d9c8deb60f5f910ca3623173797ec6a6159b16e
KanexKane <kanexkane@gmail.com>
2022-09-09 17:21:06 +0700

    set config vue router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import generateRoutes from 'virtual:generated-pages';
+import { setupLayouts } from 'virtual:generated-layouts';
+import 'windi.css';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const routes = setupLayouts(generateRoutes);
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+const app = createApp(App).use(router);
+
+Object.values(import.meta.globEager('./modules/*.js')).map((i) =>
+  i.install?.({ app })
+);
+
+app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
+import Pages from 'vite-plugin-pages';
+import Layouts from 'vite-plugin-vue-layouts';
+import VueI18n from '@intlify/vite-plugin-vue-i18n';
+import WindiCss from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  server: {
+    host: '0.0.0.0',
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7001',
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    Pages(),
+    Layouts(),
+    VueI18n({
+      compositionOnly: false,
+      include: [path.resolve(__dirname, 'locales/**')],
+    }),
+    WindiCss(),
+  ],
+});
